01fba93da23ca2085193add33ada14009a52d881
yunfulee <1113802150@qq.com>
2024-04-07 12:58:54 +0000

    feat(clean code): clean code repo

--- src/app/store/app.reducer.ts
+++
@@ -1,9 +0,0 @@
-import * as fromTransactions from '../income-expense/store/transaction-reducer';
-import { ActionReducerMap } from '@ngrx/store';
-export interface AppState {
-  transactions: fromTransactions.State;
-}
-
-export const appReducer: ActionReducerMap<AppState> = {
-  transactions: fromTransactions.TransactionReducer
-}
